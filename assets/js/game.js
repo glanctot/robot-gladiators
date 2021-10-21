5f1497f09bc9f99c992ddda0420b53e0532e240c
glanctot <gregory.lanctot28@gmail.com>
2021-10-21 13:20:17 -0500

    highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -112,12 +112,22 @@ endGame();
 
 
 var endGame = function() {
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-  window.alert("The game has now ended.  Let's see how you did!");
-} else {
-  window.alert("You've lost your robot in battle.")
-}
+ window.alert("The game has now ended.  Let's see how you did!");
+
+ var highScore = localStorage.getItem("highscore");
+ if (highScore === null) {
+   highScore = 0;
+ }
+ // if player has more money than the high score, player has new highscore
+ if (playerInfo.money > highScore) {
+   localStorage.setItem("highscore", playerInfo.money);
+   localStorage.setItem("name", playerInfo.name);
+
+   alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+ } else {
+   alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+ }
+
 // ask if they would like to play again
 var playAgainConfirm = window.confirm("Would you like to play again?");
 if (playAgainConfirm) {
